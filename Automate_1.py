602f0dfbc51441a03c319ad7e48e1c71df3a9b6c
LordVarian <starkened@gmail.com>
2024-01-06 18:37:04 -0800

    changed

--- Automate_1.py
+++ Automate_1.py
@@ -1,1 +1,11 @@
-type(2)
+# This program says hello and asks the user for their name.
+
+print("Hello, what is your name?") # ask for the user's name
+name = input()
+print("Hello, " + name + "!")
+print("The length of your name is: ")
+print(len(name))
+
+print("What is your age?") # ask for the user's age
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
---
+++ Automate_2_c.py
@@ -0,0 +1,10 @@
+name = "bob"
+age = 3000
+if name == "Alice":
+    print("Hello, Alice!")
+elif age < 12:
+    print("You are not Alice, kiddo.")
+elif age > 2000:
+    print("Unlike you, Alice is not and undead, immortal vampire")
+elif age > 100:
+    print("You are not Alice, grannie.")
