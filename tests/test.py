7db7aa377faa5901e28156e661bc2adb8ef94772
LordVarian <starkened@gmail.com>
2024-01-13 16:27:18 -0800

    finished initial Panda lessons

---
+++ tests/test.py
@@ -0,0 +1,5 @@
+def job(engineer, analyst, developer):
+    print(' // '.join([engineer, analyst, developer]))
+    return
+
+job('Open Pit Engineer 1', 'Data & Analytics Analyst', 'Business Intelligence Developer')
